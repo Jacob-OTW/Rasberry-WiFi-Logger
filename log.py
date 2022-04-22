1ac1805e7a21407e9c6e428e39de889ac73d0f8f
Jacob-OTW <erdnussflipshow@gmail.com>
2022-04-22 18:59:12 +0200

    Initial commit

---
+++ log.py
@@ -0,0 +1,42 @@
+import requests
+import csv
+import os
+import datetime
+
+
+def write(filename, row: list):  # Write to csv file
+    f = open(filename, 'a', newline='')
+    writer = csv.writer(f)
+    writer.writerow(row)
+    f.close()
+
+
+# Ping google and store result as boolean
+url = 'https://google.com'
+timeout = 10
+
+try:
+    request = requests.get(url, timeout=timeout)
+    connection = True
+except (requests.ConnectionError, requests.Timeout) as exception:
+    connection = False
+
+# Make sure file isn't emtpy
+if os.stat('all.csv').st_size != 0:
+    with open("all.csv") as file:
+        lines = file.readlines()
+    last_line_status = lines[-1].split(',')[0]
+else:
+    last_line_status = connection
+
+# Handle writing to file
+if connection:  # The ping worked
+    x = datetime.datetime.now()
+    if last_line_status == 'False':  # If last ping didn't work
+        write('off-to-on.csv', ['Reconnected: ', x.strftime("%d.%b.%Y %H:%M:%S")])
+    write('all.csv', [connection, x.strftime("%d.%b.%Y %H:%M:%S")])
+else:  # The ping didn't work
+    x = datetime.datetime.now()
+    if last_line_status == 'True':  # If last ping worked
+        write('off-to-on.csv', ['disconnected: ', x.strftime("%d.%b.%Y %H:%M:%S")])
+    write('all.csv', [connection, x.strftime("%d.%b.%Y %H:%M:%S")])
---
+++ mail.py
@@ -0,0 +1,39 @@
+import datetime
+import smtplib
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+
+def send_mail(*files):
+    password = ''
+    from_mail = ''
+    to_mail = ''
+    x = datetime.datetime.now()
+    subject = f'Log files {x.strftime("%d.%b.%Y %H:%M:%S")}'
+    body = 'Text'
+
+    msg = MIMEMultipart()
+    msg['from'] = from_mail
+    msg['to'] = to_mail
+    msg['subject'] = subject
+    body = MIMEText(body, 'plain')
+    msg.attach(body)
+
+    for file in files:
+        filename = file
+        with open(filename, 'r') as f:
+            attachment = MIMEApplication(f.read(), Name=basename(filename))
+            attachment['Content-Disposition'] = f'attachment; filename="{basename(filename)}"'
+
+        msg.attach(attachment)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.connect('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(from_mail, password)
+
+    server.send_message(msg, from_addr=from_mail, to_addrs=[to_mail])
